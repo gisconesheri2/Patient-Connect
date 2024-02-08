6997d28cbf1e99fec36fb42296c4a383676f72c6
gisconesheri2 <pgikonyo2022@gmail.com>
2024-02-09 00:53:56 +0300

    trial medic authorization

--- twilioyest.py
+++ twilioyest.py
@@ -1,13 +1,51 @@
-from twilio.rest import Client
+from html.parser import HTMLParser
+from bs4 import BeautifulSoup
+from lxml import html
+import requests
 
-account_sid = 'ACff3b87ddef71a06732596514466ccedd'
-auth_token = '8df9fb1e86872f3669442c39bb0a35fd'
-client = Client(account_sid, auth_token)
+html_doc = """ <tr >
+                                        <td >PERPETUA WANJIKU KARANJA</td>
+                                        <td >2517</td>
+                                        <td >P2024D00582</td>
+                                        <td ><span class='label label-success'>Status: Active</span> &nbsp; 2024-12-31</td>
+                                        <td >
+                                        <!--<a href='' class='printable'>View more</a>-->
+                                        <a  data-placement="top" title="Click to View Details" class="popStatus" href="#" rel='MzEyNjg=' data-toggle="modal" data-target=".details"    >View Details </a>
+                                       
+                                       <!-- <a href='#' rel='' class='view_details'>View Details</a>-->
+                                        </td>
+                                      </tr>
+                                                                            <tr >
+                                        <td >PETER MWANGI NGUMO</td>
+                                        <td >1966</td>
+                                        <td >P2024D00175</td>
+                                        <td ><span class='label label-success'>Status: Active</span> &nbsp; 2024-12-31</td>
+                                        <td >
+                                        <!--<a href='' class='printable'>View more</a>-->
+                                        <a  data-placement="top" title="Click to View Details" class="popStatus" href="#" rel='MzA4Njc=' data-toggle="modal" data-target=".details"    >View Details </a>
+                                       
+                                       <!-- <a href='#' rel='' class='view_details'>View Details</a>-->
+                                        </td>
+                                      </tr>"""
 
-message = client.messages.create(
-  from_='+12244125106',
-  body='test',
-  to='+18777804236'
-)
+with open('medics_register.html', 'r') as f:
+   html_doc_f = f.read()
 
-print(message.sid)
+ 
+
+ 
+soup = BeautifulSoup(html_doc_f, 'html.parser')
+headers = ['name', 'reg']
+all = []
+for tag in soup.find_all('tr'):
+    name = {}
+    n = 0
+    for td in tag.find_all('td'):
+        if n < 2:
+          name[headers[n]] = td.string
+          n += 1
+        if td.find('span') is not None:
+          name['status'] = tag.find('span').string
+    all.append(name)
+    # name['status'] = tag.find('span').string.split(':')[1]
+print(all)
